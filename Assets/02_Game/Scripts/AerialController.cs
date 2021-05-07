d500906d144a19c7448834ef667af1e8d4dc2d48
KOKIPC\saoar <tsurumi.kouki.as@gmail.com>
2021-05-07 22:19:30 +0900

    AreaMgrですべての足場を管理できるようにしました 詳しくは仕様書のシステム処理を見てください　byたかちゃんか

--- Assets/02_Game/Scripts/AerialController.cs
+++ Assets/02_Game/Scripts/AerialController.cs
@@ -4,31 +4,47 @@ using UnityEngine;
 
 public class AerialController : MonoBehaviour
 {
-    private Transform ChangeWorld;
-    private Vector3 CheckPos = Vector3.zero;
-    private Vector3 Moving = Vector3.zero;
+    // ステート
+    public enum AerialState
+    {
+        STATE_FRONT,
+        STATE_BACK,
+    };
+
+    // パラメータ
+    [SerializeField] private AerialState _AerialState = AerialState.STATE_FRONT;    // 現在のステート格納用、初期値は表
+    private GameObject _WorldMgr;                                                   // WorldMgrのオブジェクト情報を持っておく
+
+    // メンバ関数
+    //************************************************
+    //  現在のステートを入れ替える関数
+    //  関数を呼ぶ度に入れ替わる
+    //************************************************
+    public void ChangeState()
+    {
+        if (_AerialState == AerialState.STATE_FRONT)
+            _AerialState = AerialState.STATE_BACK;
+        else
+            _AerialState = AerialState.STATE_FRONT;
+    }
+
+    //************************************************
+    //  現在のステートを渡すGetter関数
+    //************************************************
+    public AerialState GetAerialState()
+    {
+        return _AerialState;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _WorldMgr = GameObject.Find("WorldMgr");    // WorldMgrを探して保持
     }
 
     // Update is called once per frame
     void Update()
     {
-        ChangeWorld = this.transform;
-        CheckPos = transform.position;
-        Moving = new Vector3(CheckPos.x, CheckPos.y, 0.0f);
-
-        if(CheckPos.z == 3.5f && Input.GetKeyDown(KeyCode.C))
-        {
-            Moving.z = 0.0f;
-            ChangeWorld.position = Moving;
-        } 
-        else if(CheckPos.z == 0.0f && Input.GetKeyDown(KeyCode.C))
-        {
-            Moving.z = 3.5f;
-            ChangeWorld.position = Moving;
-        }
+        
     }
 }
--- Assets/02_Game/Scripts/PlayerController.cs
+++ Assets/02_Game/Scripts/PlayerController.cs
@@ -26,12 +26,14 @@ public class PlayerController : MonoBehaviour
     public Vector3 _DefaultPos  = new Vector3(0.0f, 0.0f, 0.0f);    // プレイヤーの初期位置
     public bool _IsFront        = true;                             // 表ステージかどうか
 
-    private CharacterController _Controller;         // コンポーネントの取得
-    private Vector3 _MoveDirection = Vector3.zero;   // キャラクターの移動量
-    private float _H;                                // キー入力取得用
-    private CharactorState _CharactorState;          // キャラクターステート
-    private GameObject _TMPAerial;
-    
+    private CharacterController _Controller;        // コンポーネントの取得
+    private Vector3 _MoveDirection = Vector3.zero;  // キャラクターの移動量
+    private float _H;                               // キー入力取得用
+    private CharactorState _CharactorState;         // キャラクターステート
+    //private GameObject _TMPAerial;
+    private GameObject _WorldMgr;                   // WorldMgrの情報格納用
+    private GameObject _AerialMgr;                  // AerialMgrの情報格納用
+
 
     // メンバ関数
     //************************************************
@@ -56,8 +58,8 @@ public class PlayerController : MonoBehaviour
         }
         else if(s == CharactorState.STATE_WARP)// ワープ状態
         {
-            _Controller.enabled = false;     // CharactorController無効化
-            _MoveDirection = Vector3.zero;   // 移動量をゼロにする
+            _Controller.enabled = false;        // CharactorController無効化
+            _MoveDirection      = Vector3.zero; // 移動量をゼロにする
         }
     }
 
@@ -79,8 +81,15 @@ public class PlayerController : MonoBehaviour
         // CharactorStateの初期化
         _CharactorState = CharactorState.STATE_NORMAL;   // 初期値を表に設定
 
-        _TMPAerial = GameObject.Find("Aerial");
-    }
+        //_TMPAerial = GameObject.Find("Aerial");
+
+        // WorldMgrの取得
+        _WorldMgr = GameObject.Find("WorldMgr");
+
+        // WorldMgrの取得
+        _AerialMgr = GameObject.Find("AerialMgr");
+
+    }//Start
 
     // Update is called once per frame
     void Update()
@@ -109,7 +118,6 @@ public class PlayerController : MonoBehaviour
             if (_Controller.isGrounded)// キャラクターが地面についているとき
             {
                 _MoveDirection = new Vector3(_H, 0.0f, 0.0f);                   // キー入力でx成分のみ移動量に加える
-                //_MoveDirection = transform.TransformDirection(_MoveDirection);
                 _MoveDirection *= _Speed;                                       // キャラクターの設定スピードを乗算
 
                 // ジャンプ
@@ -121,47 +129,52 @@ public class PlayerController : MonoBehaviour
                 // 表と裏の変更
                 if (Input.GetKeyDown(KeyCode.Z))
                 {
-                    if (GameObject.Find("WorldMgr").GetComponent<WorldMgr>().GetWorldState() == WorldMgr.WorldState.STATE_FRONT)
+                    if (_WorldMgr.GetComponent<WorldMgr>().GetWorldState() == WorldMgr.WorldState.STATE_FRONT)
                     {
-                        GameObject.Find("WorldMgr").GetComponent<WorldMgr>().SetWorldState(WorldMgr.WorldState.STATE_BACK);
+                        _WorldMgr.GetComponent<WorldMgr>().SetWorldState(WorldMgr.WorldState.STATE_BACK);
                     }
                     else
                     {
-                        GameObject.Find("WorldMgr").GetComponent<WorldMgr>().SetWorldState(WorldMgr.WorldState.STATE_FRONT);
+                        _WorldMgr.GetComponent<WorldMgr>().SetWorldState(WorldMgr.WorldState.STATE_FRONT);
                     }
                 }
 
                 // 足場の変更（簡易実装）
                 if(Input.GetKeyDown(KeyCode.C))
                 {
-                    if (GameObject.Find("WorldMgr").GetComponent<WorldMgr>().GetWorldState() == WorldMgr.WorldState.STATE_FRONT)
-                    {
-                        if(GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().GetAerialState() == TMPAerialController.AerialState.STATE_FRONT)
-                        {
-                            GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().SetAerialState(TMPAerialController.AerialState.STATE_BACK);
-                        }
-                    }
-                    else
-                    {
-                        if(GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().GetAerialState() == TMPAerialController.AerialState.STATE_BACK)
-                        {
-                            GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().SetAerialState(TMPAerialController.AerialState.STATE_FRONT);
-                        }
-                    }
+                    // プレイヤーに最も近い足場のオブジェクトを取得
+                    GameObject aerialObj = _AerialMgr.GetComponent<AerialMgr>().GetNearestAerial();
+
+                    if (aerialObj != null)
+                        // 取得したオブジェクトのステート（表裏）を切り替える
+                        aerialObj.GetComponent<AerialController>().ChangeState();
+
+
+
+                    //if (GameObject.Find("WorldMgr").GetComponent<WorldMgr>().GetWorldState() == WorldMgr.WorldState.STATE_FRONT)
+                    //{
+                    //    if(GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().GetAerialState() == TMPAerialController.AerialState.STATE_FRONT)
+                    //    {
+                    //        GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().SetAerialState(TMPAerialController.AerialState.STATE_BACK);
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    if(GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().GetAerialState() == TMPAerialController.AerialState.STATE_BACK)
+                    //    {
+                    //        GameObject.Find("AerialMgr").GetComponent<TMPAerialController>().SetAerialState(TMPAerialController.AerialState.STATE_FRONT);
+                    //    }
+                    //}
                 }
             }
             else if(!_Controller.isGrounded)
             {
                 _MoveDirection.x = _H;                                          // キー入力でx成分のみ移動量に加える
-                //_MoveDirection = transform.TransformDirection(_MoveDirection);
                 _MoveDirection.x *= _Speed;                                     // キャラクターの設定スピードを乗算
 
                 Debug.Log("a");
             }
-
-            // 慣性
-            //_MoveDirection.x -= _Rate * Time.deltaTime;
-
+            
             // 重力設定
             _MoveDirection.y -= _Gravity * Time.deltaTime;
             _Controller.Move(_MoveDirection * Time.deltaTime);
