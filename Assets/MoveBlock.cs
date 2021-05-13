0f4fef99e02d0941f9270c1b5dfa19958c01c57b
KOKIPC\saoar <tsurumi.kouki.as@gmail.com>
2021-05-13 11:10:54 +0900

    背景をもっとぼかして、動く床のスクリプトとアイコンを追加しました

---
+++ Assets/MoveBlock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+
+    public float length = 2f;
+
+    private Rigidbody rb;
+    private Vector3 Pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(new Vector3(Pos.x, Pos.y + Mathf.PingPong(Time.time, length), Pos.z));
+    }
+
+}
